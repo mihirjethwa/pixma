c12ee67a64ee422708f887c2020a0b5f5b6e8006
mihirjethwa <40630615+mihirjethwa@users.noreply.github.com>
2019-04-02 02:24:12 +0530

    config dep

--- myproject/settings.py
+++ myproject/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'd91bp0su8qeqkg',
         'USER' : 'gqozjxmimrmjvc',
         'PASSWORD' : '6620476a3ebf3309fd9f8e30515ae92d3fe51c16a778975760fe4cf5dbf4ccb7',
