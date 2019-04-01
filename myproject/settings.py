bd186b2105bff688de27492456be59cdde0e01bf
mihirjethwa <40630615+mihirjethwa@users.noreply.github.com>
2019-04-02 02:14:28 +0530

    config dep

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,10 +81,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pixma',
-        'USER' : 'root',
-        'PASSWORD' : 'password',
-        'HOST' : 'localhost'
+        'NAME': 'd91bp0su8qeqkg',
+        'USER' : 'gqozjxmimrmjvc',
+        'PASSWORD' : '6620476a3ebf3309fd9f8e30515ae92d3fe51c16a778975760fe4cf5dbf4ccb7',
+        'HOST' : '392691ec2-50-17-246-114.compute-1.amazonaws.com'
+        'PORT': '5432'
     }
 }
 
