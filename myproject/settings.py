d0f625d30320ae5c03b9e436eafd5edb24cab1ee
mihirjethwa <40630615+mihirjethwa@users.noreply.github.com>
2019-04-02 02:25:40 +0530

    config dep

--- myproject/settings.py
+++ myproject/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'USER' : 'gqozjxmimrmjvc',
         'PASSWORD' : '6620476a3ebf3309fd9f8e30515ae92d3fe51c16a778975760fe4cf5dbf4ccb7',
         'HOST' : 'ec2-50-17-246-114.compute-1.amazonaws.com',
-        'PORT' : '5432'
+        'PORT' : '5432',
     }
 }
 
